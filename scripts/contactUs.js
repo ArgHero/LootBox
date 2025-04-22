ceb54831cdb5f051655393f539baed5f9efa0a4c
ArgHero <saul.hernandez.ipn@gmail.com>
2025-04-22 15:43:38 -0600

    validaciones de correo y telefono

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -1,8 +1,16 @@
-let txtNombre = document.getElementById("txtNombre");
-let txtMensaje = document.getElementById("txtMensaje");
-let btnEnviar = document.getElementById("btnEnviar");
-let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
-let alertValidaciones = document.getElementById("alertValidaciones");
+const telPattern = new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
+const emailPattern = new RegExp("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+");
+
+const txtCorreo = document.getElementById("txtCorreo");
+const txtTel = document.getElementById("txtTel");
+const txtNombre = document.getElementById("txtNombre");
+const txtMensaje = document.getElementById("txtMensaje");
+const btnEnviar = document.getElementById("btnEnviar");
+const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const alertValidaciones = document.getElementById("alertValidaciones");
+
+let correoValido = false;
+let telefonoValido = false;
 
 // Se valida el nombre
 function validarNombre() {
@@ -11,12 +19,23 @@ function validarNombre() {
   return partesNombre.length >= 2 && partesNombre.every(p => p.length > 0);
 }//validarNombre
 
-
 // Se valida el mensaje
 function validarMensaje() {
   const longitud = txtMensaje.value.trim().length;
   return longitud >= 10 && longitud <= 150;
 }//ValidarMensahe
+
+// Validar correo electrónico
+function validarEmail() {
+  const content = txtCorreo.value.trim();
+  return emailPattern.test(content);
+};//validarEmail()
+
+// Validar correo electrónico
+function validarTelefono() {
+  const content = txtTel.value.trim();
+  return telPattern.test(content);
+};//validarTelefono()
 
 // Botón enviar
 btnEnviar.addEventListener("click", function(event) {
@@ -62,9 +81,49 @@ btnEnviar.addEventListener("click", function(event) {
     // Limpiar los campos después de enviar
     txtNombre.value = "";
     txtMensaje.value = "";
+    txtCorreo.value = "";
+    txtTel.value = "";
+
     txtNombre.focus();
 
     // Ocultar la alerta de validaciones ya que todo está bien
     alertValidaciones.style.display = "none";
   }
-});
+});//btnEnviar-Click
+
+//---------LimpiarContorno----
+const cleanBorder = (event)=>{
+  const contenedor = event.target;
+  contenedor.style.border = "";
+};//cleanBorder()
+
+const colorBorder = (event)=>{
+  const contenedor = event.target;
+  contenedor.style.border = "2px solid red";
+};//colorBorder()
+
+txtNombre.addEventListener("click",cleanBorder);
+txtMensaje.addEventListener("click",cleanBorder);
+txtTel.addEventListener("click",cleanBorder);
+txtCorreo.addEventListener("click",cleanBorder);
+
+//----------Validación rápida-----------------
+txtNombre.addEventListener("blur",(event)=>{
+  if(!validarNombre())
+    colorBorder(event);
+});//txtNombre-blur
+
+txtMensaje.addEventListener("blur",(event)=>{
+  if(!validarMensaje())
+    colorBorder(event);
+});//txtMensaje-blur
+
+txtTel.addEventListener("blur",(event)=>{
+  if(!validarTelefono())
+    colorBorder(event);
+});//txtTel-blur
+
+txtCorreo.addEventListener("blur",(event)=>{
+  if(!validarEmail())
+    colorBorder(event);
+});//txtCorreo-blur
