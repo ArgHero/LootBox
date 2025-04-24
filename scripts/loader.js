c7cd31cae8eb5ddcfa95ac007a163736374586dc
Felipe Rosas <felipe.rosas1199@gmail.com>
2025-04-24 13:08:19 -0600

    Se agrego script para navbar y se agrego carpeta de loader con navbar.hmtl

---
+++ scripts/loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('./load/navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error cargando el navbar:', error);
+      });
+  });
