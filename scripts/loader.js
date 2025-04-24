7df7c660b6b092566a3d92fbf986aed40a56dd65
ArgHero <saul.hernandez.ipn@gmail.com>
2025-04-24 14:06:46 -0600

    cargar footer

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('./load/navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-      })
-      .catch(error => {
-        console.error('Error cargando el navbar:', error);
-      });
-  });
+const navPath = "./assets/components/navbar.html";
+const footPath = "./assets/components/footer.html";
+
+const navBar=document.getElementById("navbar-container");
+const cuerpo=document.getElementById("footer-container");
+
+document.addEventListener("DOMContentLoaded", loadContainers);
+
+function loadContainers(){
+  fetch(navPath)
+    .then(response => response.text())
+    .then(data => navBar.innerHTML = data)
+    .catch(error => console.error('Error cargando el navbar:', error));
+  fetch(footPath)
+    .then(response => response.text())
+    .then(data => cuerpo.innerHTML = data)
+    .catch(error => console.error('Error cargando el footer:', error));
+}
