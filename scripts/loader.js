d8533959dc6688000548142048a4966e1119d643
ArgHero <saul.hernandez.ipn@gmail.com>
2025-04-24 15:53:01 -0600

    función de resaltar

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,10 +1,9 @@
 const navPath = "./assets/components/navbar.html";
 const footPath = "./assets/components/footer.html";
+const nombrePagina = window.location.pathname.split('/').pop();
 
 const navBar=document.getElementById("navbar-container");
 const cuerpo=document.getElementById("footer-container");
-
-document.addEventListener("DOMContentLoaded", loadContainers);
 
 function loadContainers(){
   fetch(navPath)
@@ -15,4 +14,13 @@ function loadContainers(){
     .then(response => response.text())
     .then(data => cuerpo.innerHTML = data)
     .catch(error => console.error('Error cargando el footer:', error));
-}
+};//loadContainers()
+
+function resaltarNav(){
+  console.log("resaltación");
+  
+};//resaltarNav()
+
+
+document.addEventListener("DOMContentLoaded", loadContainers);
+window.addEventListener("load",resaltarNav);
