89dfde24cf8ae0ea10a1d122df5caedfd7ee33c5
ArgHero <saul.hernandez.ipn@gmail.com>
2025-04-24 16:52:41 -0600

    Focus en el navBar

--- scripts/loader.js
+++ scripts/loader.js
@@ -8,7 +8,10 @@ const cuerpo=document.getElementById("footer-container");
 function loadContainers(){
   fetch(navPath)
     .then(response => response.text())
-    .then(data => navBar.innerHTML = data)
+    .then(data => {
+      navBar.innerHTML = data;
+      resaltarNav();
+    })
     .catch(error => console.error('Error cargando el navbar:', error));
   fetch(footPath)
     .then(response => response.text())
@@ -17,10 +20,11 @@ function loadContainers(){
 };//loadContainers()
 
 function resaltarNav(){
-  console.log("resaltación");
-  
+  const paginas = ["index.html","aboutUs.html","contacUs.html","prodList.html","publicar.html"];
+  const navFocus = document.getElementById("navbar-container").getElementsByClassName("nav-item")[paginas.indexOf(nombrePagina)].getElementsByTagName("a").item(0);
+  navFocus.classList.add("active");
 };//resaltarNav()
 
 
 document.addEventListener("DOMContentLoaded", loadContainers);
-window.addEventListener("load",resaltarNav);
+
