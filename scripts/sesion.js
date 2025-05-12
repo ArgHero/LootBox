99ab019d1cf4c2bbc37d1d2fd8c3f37431be7d02
GMOS <gabriela.m.ordonezs@gmail.com>
2025-05-12 12:33:56 -0600

    Se agrega la validación para el inicio de sesión

---
+++ scripts/sesion.js
@@ -0,0 +1,69 @@
+const txtCorreo = document.getElementById("txtCorreo");
+const txtPassword = document.getElementById("txtPassword");
+const btnEnviar = document.getElementById("btnEnviar");
+const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const alertValidaciones = document.getElementById("alertValidaciones");
+
+
+// Iniciar sesión
+btnEnviar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    alertValidaciones.style.display = "none";
+    alertValidacionesTexto.innerHTML = "";
+    txtCorreo.style.border = "";
+    txtPassword.style.border = "";
+    let isValid = true;
+
+    // Validación campos vacíos
+    const correo = txtCorreo.value.trim();
+    if (correo === "") {
+        txtCorreo.style.border = "2px solid red";
+        alertValidacionesTexto.innerHTML += "<strong>El campo Correo no puede estar vacío.</strong><br/><br/>";
+        isValid = false;
+    }
+
+    const password = txtPassword.value.trim();
+    if (password === "") {
+        txtPassword.style.border = "2px solid red";
+        alertValidacionesTexto.innerHTML += "<strong>La contraseña no puede estar vacía.</strong><br/><br/>";
+        isValid = false;
+    }
+
+    if (!isValid) {
+        alertValidaciones.style.display = "block";
+        return;
+    }
+
+    // Verificación de usuario
+    const usuarios = JSON.parse(localStorage.getItem('users')) || [];
+    const usuarioEncontrado = usuarios.find(usuario =>
+        usuario.email.toLowerCase() === correo.toLowerCase() &&
+        usuario.password === password
+    );
+
+    if (usuarioEncontrado) {
+        Swal.fire("Bienvenido", `Hola ${usuarioEncontrado.nombre}`, "success").then(()=>{
+            window.location.href = "./index.html";
+        });
+    } else {
+        const correoExiste = usuarios.some(usuario => usuario.email.toLowerCase() === correo.toLowerCase());
+
+        if(correoExiste){
+            Swal.fire("Error", "Contraseña incorrecta", "error");
+        } else {
+            Swal.fire({
+                title: "Correo no registrado",
+                text: "Este correo no está registrado. ¿Deseas crear una cuenta?",
+                showCancelButton: true,
+                confirmButtonText: "Ir al registro",
+                cancelButtonText: "Cancelar"
+            }).then((result)=> {
+                if(result.isConfirmed) {
+                    window.location.href ="./registro.html"
+                }
+            })  
+        }
+        
+    }
+});
