b909190b514f67314b98b3ab9f75c862fc41046b
ArgHero <saul.hernandez.ipn@gmail.com>
2025-05-27 20:43:09 -0600

    implementación del cloudinary

--- scripts/publicar.js
+++ scripts/publicar.js
@@ -11,16 +11,18 @@ const urlImage = document.getElementById("url-imagen");
 const btnPublicar = document.getElementById("btn-publicar");
 const selectorCategoria = document.getElementById("selector-categoria");
 //-Cargar Imagenes
-const fileInput = document.getElementById("fileUpload");
 const fileImageContainer = document.getElementById("fileImageContainer");
-//const imageOutput = document.getElementById("output");
-const spinner = document.getElementsByClassName("spinner-border").item(0);
-let imageCoded;
+const imageOutput = document.getElementById("output");
+const uploadBtn = document.getElementById("upload_widget");
+const img_fileName = document.getElementById("img_fileName");
+//const spinner = document.getElementsByClassName("spinner-border").item(0);
+
 
 const tablaItems = document.getElementById("tabla-items");
 const cuerpoTabla = tablaItems.getElementsByTagName("tbody").item(0);
 
 let listaProductos = [];
+let imageCoded;
 
 function mostrarDatosLocal(){
   listaProductos = JSON.parse(localStorage.getItem('productos')) || [];
@@ -95,14 +97,11 @@ function validarCamposProducto({ sku, producto, descripcion, precio, costo, cant
     mostrarError(cantidad, 'cantidad-alert-container', 'La cantidad debe ser un número mayor que 0 y sin decimales.');
     hayErrores = true;
   }
-
   
-  if (!fileInput.files.length) {
+  if (!img_fileName.innerText) {
     mostrarError(fileImageContainer, 'fileUpload-alert-container', 'La imagen del producto es necesaria.');
     hayErrores = true;
   }
-  
-
   return !hayErrores;
 }
 
@@ -118,7 +117,7 @@ btnPublicar.addEventListener("click", function (event) {
     precio,
     costo,
     cantidad,
-    fileInput
+    imageOutput
   });
 
   if (!valido) return;
@@ -411,26 +410,26 @@ function deleteComp(event){
 
 
 //----Cargar imágenes
-fileInput.addEventListener("change", async () => {
-  imageCoded = null;
-  spinner.classList.remove("visually-hidden");
-  let [file] = fileInput.files;
+const myWidget = cloudinary.createUploadWidget({
 
-  const reader = new FileReader();
-  reader.onload = (event) => {
-    //imageOutput.src = event.target.result;
-    imageCoded = event.target.result;
-    spinner.classList.add("visually-hidden");
-  };
+    cloudName: 'dsjcemt6v', // Reemplaza con tu Cloud Name de Cloudinary
+    uploadPreset: 'LootBox', // Reemplaza con tu Upload Preset (debes crearlo en tu consola de Cloudinary)
+    sources: ['local', 'url'], // Fuentes permitidas para la subida
+    multiple: false, // Permitir una sola subida o múltiples
+    cropping: true, // Habilitar la herramienta de recorte
+    clientAllowedFormats: ['png', 'gif', 'jpeg','webp']
 
-  reader.onerror = (err) => {
-      console.error("Error reading file:", err);
-      alert("An error occurred while reading the file.");
-      spinner.classList.add("visually-hidden");
-  };
-  reader.readAsDataURL(file);
+}, (error, result) => {
+    imageCoded = null;
+    if (!error && result && result.event === "success") {
+        console.log(result);
+        imageCoded = result.info.secure_url;
+        imageOutput.src=result.info.thumbnail_url;
+        img_fileName.innerText=result.info.original_filename;
+    }
 });
 
+uploadBtn.addEventListener("click", ()=>myWidget.open());
 //-----
 document.addEventListener("DOMContentLoaded", mostrarDatosLocal);
 //----Cargar tabla si no existia el local storage
